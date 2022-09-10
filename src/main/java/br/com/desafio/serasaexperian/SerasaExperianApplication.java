9bb2e9de75d4e72e943041e06c506c14b19c7ceb
rodrigoantoniocruz <rodrigoantoniocruz134@gmail.com>
2022-09-10 15:55:18 -0300

    Criação do arquétipo do projeto

---
+++ src/main/java/br/com/desafio/serasaexperian/SerasaExperianApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.serasaexperian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerasaExperianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerasaExperianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafio/serasaexperian/SerasaExperianApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.serasaexperian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerasaExperianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
